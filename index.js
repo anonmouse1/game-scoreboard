f05ef858d3cacd87e4cba5355b7e8a2dc0694a62
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2022-12-29 22:32:01 +1100

    css begin

---
+++ index.js
@@ -0,0 +1,7 @@
+let homeScore = document.getElementById("home-score");
+
+let guestScore = document.getElementById("guest-score");
+
+/* initialise values */
+homeScore.textContent = 0;
+guestScore.textContent = 0;
