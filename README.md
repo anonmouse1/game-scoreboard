34e4e917e24c7b19cdb86201b16bef41deca3099
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2022-12-30 15:58:13 +1100

    finished product test readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# Scoreboard style game score keeping system
+# GAA scoreboard
+
+A very basic implementation of a GAA (Gaelic Athletics Association) scoreboard system that can be used to keep track of scores in gaelic sports such as hurling,football or camogie.
+
+- 1 goal(under crossbar and between posts) = 3 points
+- 1 point(over crossbar and between posts) = 1 point
 
 Quick start:
 
@@ -6,3 +11,5 @@ Quick start:
 $ npm install
 $ npm start
 ```
+
+[Webpage](gaaScoreboard.png)
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let homeScore = document.getElementById("home-score");
 let guestScore = document.getElementById("guest-score");
+let homeTotalScore = document.getElementById("home-total");
+let guestTotalScore = document.getElementById("guest-total");
 
 /* initialise values */
 homeScore.textContent = 0;
 guestScore.textContent = 0;
+homeTotalScore.textContent = "( " + 0 + " )";
+guestTotalScore.textContent = "( " + 0 + " )";
+
+let gameNumber = 1;
 
 let homeGoals = 0;
 let homePoints = 0;
@@ -17,22 +23,41 @@ function homeScoreGoal() {
   homeGoals += 1;
   homeScore.textContent = homeGoals + " . " + homePoints;
   homeTotal += 3;
+  homeTotalScore.textContent = "( " + homeTotal + " )";
 }
 
 function homeScorePoint() {
   homePoints += 1;
   homeScore.textContent = homeGoals + " . " + homePoints;
   homeTotal += 1;
+  homeTotalScore.textContent = "( " + homeTotal + " )";
 }
 
 function guestScoreGoal() {
   guestGoals += 1;
   guestScore.textContent = guestGoals + " . " + guestPoints;
   guestTotal += 3;
+  guestTotalScore.textContent = "( " + guestTotal + " )";
 }
 
 function guestScorePoint() {
   guestPoints += 1;
   guestScore.textContent = guestGoals + " . " + guestPoints;
-  guestTotal += 3;
+  guestTotal += 1;
+  guestTotalScore.textContent = "( " + guestTotal + " )";
+}
+
+function nextGame() {
+  homeGoals = 0;
+  homePoints = 0;
+  homeTotal = 0;
+  homeScore.textContent = homeGoals + " . " + homePoints;
+  homeTotalScore.textContent = "( " + homeTotal + " )";
+
+  guestGoals = 0;
+  guestPoints = 0;
+  guestTotal = 0;
+
+  guestScore.textContent = guestGoals + " . " + guestPoints;
+  guestTotalScore.textContent = "( " + 0 + " )";
 }
