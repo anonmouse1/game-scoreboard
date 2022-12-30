e1809ce085b1d3d2bc42a5329947a066776f4ffd
anonmouse1 <90865869+anonmouse1@users.noreply.github.com>
2022-12-30 12:20:07 +1100

    basic style working goals/points

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 let homeScore = document.getElementById("home-score");
-
 let guestScore = document.getElementById("guest-score");
 
 /* initialise values */
 homeScore.textContent = 0;
 guestScore.textContent = 0;
+
+let homeGoals = 0;
+let homePoints = 0;
+let homeTotal = 0;
+
+let guestGoals = 0;
+let guestPoints = 0;
+let guestTotal = 0;
+
+function homeScoreGoal() {
+  homeGoals += 1;
+  homeScore.textContent = homeGoals + " . " + homePoints;
+  homeTotal += 3;
+}
+
+function homeScorePoint() {
+  homePoints += 1;
+  homeScore.textContent = homeGoals + " . " + homePoints;
+  homeTotal += 1;
+}
+
+function guestScoreGoal() {
+  guestGoals += 1;
+  guestScore.textContent = guestGoals + " . " + guestPoints;
+  guestTotal += 3;
+}
+
+function guestScorePoint() {
+  guestPoints += 1;
+  guestScore.textContent = guestGoals + " . " + guestPoints;
+  guestTotal += 3;
+}
